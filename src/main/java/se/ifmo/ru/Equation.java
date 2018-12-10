98dba55bb8e980c015b9a0dfb86fa301335f8579
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-12-10 12:27:26 +0300

    fix first equation

--- src/main/java/se/ifmo/ru/Equation.java
+++ src/main/java/se/ifmo/ru/Equation.java
@@ -4,7 +4,7 @@ import se.ifmo.ru.interpolation.Function;
 
 public enum Equation {
 
-    FIRST("y'= y * x", Function.FIRST), SECOND("y' = e^x + y", Function.SECOND), THIRD("y' = xy^2", Function.THIRD);
+    FIRST("y'= y + x", Function.FIRST), SECOND("y' = e^x + y", Function.SECOND), THIRD("y' = xy^2", Function.THIRD);
 
     private String equation;
     private Function function;
@@ -17,7 +17,7 @@ public enum Equation {
     public double calculateFunction(double x, double y) {
         switch (this) {
             case FIRST:
-                return (y * x); //y0 = 0
+                return (y + x); //y0 = 1
             case SECOND:
                 return (Math.pow(Math.E, x) * y); // y(0) = 1
             case THIRD:
